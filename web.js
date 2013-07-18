172a2879ba6f0490d87e395cd7bcec72330a6dd7
Ubuntu <ubuntu@ip-172-31-21-203.us-west-2.compute.internal>
2013-07-18 21:14:38 +0000

    updated H4 index.html

--- web.js
+++ web.js
@@ -12,7 +12,9 @@ app.get('/', function(request, response) {
 
 });
 
+
 var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/assets'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
